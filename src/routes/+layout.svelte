65ad5f5a11147ac8a40064b1a2b95bd7203839ea
Honza Led <honzaled@proton.me>
2023-06-23 10:31:58 +0000

    Setup tailwind, add some text

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,16 @@
+<script>
+  import "../app.css";
+</script>
+
 <nav>
   <a href="/">home</a>
   <a href="/about">about</a>
 </nav>
 
 <slot />
+
+<style lang="postcss">
+  :global(body) {
+    @apply bg-white dark:bg-black;
+  }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,6 @@
-<h1>home</h1>
-<p>this is the home page.</p>
+<div class="columns-2">
+    <h1 class="mx-auto dark:text-gray-100 text-gray-900">Hi, I'm HonzaLed <br>
+        Photographer and Python and web developer
+    </h1>
+    <img class="w-56" src="/waving-hand.svg" alt="Waving hand emoticon">
+</div>
